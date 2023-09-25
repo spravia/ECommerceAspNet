c9d586fb22d93d8d16a6efae0b65cd4c4afba536
Savas Pravia Vega <sabaspravia@hotmail.com>
2023-09-25 11:32:45 -0600

    test

---
+++ Database/Models/Roles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace Database.Models
+{
+    [Table("Roles")]
+    public class Roles
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string NombreRole { get; set; }
+
+        public string Image { get; set; }
+
+        public string Route{ get; set;}
+
+        public int IdUsuarioECommerce { get; set; }
+        public UsuarioECommerceTable? UsuarioECommerceTable { get; set; }
+
+        
+    }
+}
+
+
---
+++ Database/Models/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class UserData
+    {
+
+        public int id  { get; set; }
+
+        public string nombres { get; set; }
+
+        public string apellidos { get; set; }   
+       
+        public string telefono { get; set; }    
+
+        public string roles { get; set; }
+    }
+}
